57997767c03e8da0297fbb8e242f9c7b76c0c44a
Angel <a25045@universidad-une.com>
2024-01-29 08:43:12 -0600

    Tarea

---
+++ Python/Listas.py
@@ -0,0 +1,40 @@
+nombre_de_la_Variable = []
+metodo2 = List('ABCDE')
+
+print (metodo2)
+
+for i in 'CADENA DE CARACTERS':
+    print (i)
+}
+    Lista = range(100) # range(a,b,c) (a,b)
+    print (list(Lista))
+
+    for i in Lista # for i in range(100)
+    print(i)
+    
+    x = list(range(1, 11, 2)) # for (int i = 1,) i < 10, i += 2)
+    
+    for i in range(in(x)): # length 
+    print(3, 9[i])
+
+    for i, elegante in enumerate (x):
+    print (i, elegante)
+
+    otraLista = list('Angel')
+    elemento = otraLista[0] + otraLista[1] +otraLista[3]
+    elemento = otraLista[:4]
+    otraLista.reverse()
+    elemento = otraLista[4:]
+    elemento = otraLista[0-4:3]
+    elemento = otraLista[::-1]
+    print (elemento)
+
+    for i in otraLista:
+    print (i[11]-1)
+     holder = ''
+     for i in range
+
+    ListaVacia = []
+    for i in range (50):
+   ListaVacia
+   
